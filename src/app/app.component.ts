ac89411ce6948293ef38c26d0f2c6849b25dbad4
Mohammad Rahhal <m.r992@hotmail.com>
2017-08-12 10:21:26 +0300

    Rename sass -> scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	template: `<app-layout></app-layout>`,
-	styleUrls: ['./sass/app.scss'],
+	styleUrls: ['./scss/app.scss'],
 	encapsulation: ViewEncapsulation.None,
 })
 export class App {
