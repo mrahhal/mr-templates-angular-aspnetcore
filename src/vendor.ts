9ac1abea035fcf812f2d951ec75902a5b7cd9768
Mohammad Rahhal <m.r992@hotmail.com>
2017-08-31 18:46:11 +0300

    More rx ops

--- src/vendor.ts
+++ src/vendor.ts
@@ -12,12 +12,17 @@ import 'moment';
 
 import 'rxjs/Observable';
 import 'rxjs/Subject';
+import 'rxjs/add/observable/combineLatest';
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/finally';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/toPromise';
